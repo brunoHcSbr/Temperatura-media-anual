765c068a81f411beb6e3529507795ca60f7b34f3
Bruno Henrique <bh089188@gmail.com>
2022-11-16 15:50:11 -0300

    atv 5

---
+++ js/script.js
@@ -0,0 +1,93 @@
+function mediaTemp(){
+    const janeiro = prompt("Qual a temperatura média de Janeiro?")
+    const fevereiro = prompt("Qual a temperatura média de Fevereiro?")
+    const marco = prompt("Qual a temperatura média de Março?")
+    const abril = prompt("Qual a temperatura média de Abril?")
+    const maio = prompt("Qual a temperatura média de Maio?")
+    const junho = prompt("Qual a temperatura média de Junho?")
+    const julho = prompt("Qual a temperatura média de Julho?")
+    const agosto = prompt("Qual a temperatura média de Agosto?")
+    const setembro = prompt("Qual a temperatura média de Setembro?")
+    const outubro = prompt("Qual a temperatura média de Outubro?")
+    const novembro = prompt("Qual a temperatura média de Novembro?")
+    const dezembro = prompt("Qual a temperatura média de Dezembro?")
+
+    const media = (parseFloat(janeiro)+parseFloat(fevereiro)+parseFloat(marco)+parseFloat(abril)+parseFloat(maio)+parseFloat(junho)+parseFloat(julho)+parseFloat(agosto)+parseFloat(setembro)+parseFloat(outubro)+parseFloat(novembro)+parseFloat(dezembro))/12;
+    console.log(media);
+
+    const mediaMeses = document.getElementById('mediaMeses');
+    const jan = document.getElementById('jan');
+    const fev = document.getElementById('fev');
+    const marc = document.getElementById('marc');
+    const abrl = document.getElementById('abril');
+    const mai = document.getElementById('maio');
+    const jun = document.getElementById('jun');
+    const jul = document.getElementById('jul');
+    const agost = document.getElementById('agosto');
+    const setemb = document.getElementById('setemb');
+    const out = document.getElementById('out');
+    const novem = document.getElementById('novem');
+    const dezem = document.getElementById('dezem');
+    const med = document.getElementById('med');
+    
+    const tdJan = document.createElement('td');
+    tdJan.innerText = janeiro;
+    const tdFev = document.createElement('td');
+    tdFev.innerText = fevereiro;
+    const tdMarc = document.createElement('td');
+    tdMarc.innerText = marco;
+    const tdAbrl = document.createElement('td');
+    tdAbrl.innerText = abril;
+    const tdMai = document.createElement('td');
+    tdMai.innerText = maio;
+    const tdJun = document.createElement('td');
+    tdJun.innerText = junho;
+    const tdJul = document.createElement('td');
+    tdJul.innerText = julho;
+    const tdAgost = document.createElement('td');
+    tdAgost.innerText = agosto;
+    const tdSetemb = document.createElement('td');
+    tdSetemb.innerText = setembro;
+    const tdOut = document.createElement('td');
+    tdOut.innerText = outubro;
+    const tdNovem = document.createElement('td');
+    tdNovem.innerText = novembro;
+    const tdDezem = document.createElement('td');
+    tdDezem.innerText = dezembro;
+    const tdMed = document.createElement('td');
+    tdMed = media
+
+    jan.appendChild(tdJan);
+    fev.appendChild(tdFev);
+    marc.appendChild(tdMarc);
+    abrl.appendChild(tdAbrl);
+    mai.appendChild(tdMai);
+    jul.appendChild(tdJul);
+    jun.appendChild(tdJun);
+    agost.appendChild(tdAgost);
+    setemb.appendChild(tdSetemb);
+    out.appendChild(tdOut);
+    novem.appendChild(tdNovem);
+    dezem.appendChild(tdDezem);
+    /*const alunos = document.getElementById('alunos');
+    const tr = document.createElement('tr');
+    const tdNome = document.createElement('td');
+    tdNome.innerText = nome;
+    const tdMatematica = document.createElement('td');
+    tdMatematica.innerText = matematica;
+    const tdPortugues = document.createElement('td');
+    tdPortugues.innerText = portugues;
+    const tdCiencia = document.createElement('td');
+    tdCiencia.innerText = ciencia;
+    const tdMedia = document.createElement('td');
+    tdMedia.innerText = media;
+
+    tr.appendChild(tdNome);
+    tr.appendChild(tdMatematica);
+    tr.appendChild(tdPortugues);
+    tr.appendChild(tdCiencia);
+    tr.appendChild(tdMedia);
+    alunos.appendChild(tr);*/
+}
+document.getElementById('add').addEventListener('click',mediaTemp);
+//window.addEventListener('load',mediaTemp);
